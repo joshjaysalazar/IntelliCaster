3779883f4d3c4ad696010ecbec6224828503500a
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-10-02 12:25:01 -0500

    Made more progress on generating text and audio.

---
+++ test/audio_gen.py
@@ -0,0 +1,20 @@
+from transformers import AutoProcessor, BarkModel
+import scipy
+import os
+
+
+os.environ["SUNO_OFFLOAD_CPU"] = "True"
+os.environ["SUNO_USE_SMALL_MODELS"] = "True"
+
+processor = AutoProcessor.from_pretrained("suno/bark")
+model = BarkModel.from_pretrained("suno/bark")
+
+voice_preset = "v2/en_speaker_6"
+
+inputs = processor("Hello, my dog is cute", voice_preset=voice_preset)
+
+audio_array = model.generate(**inputs)
+audio_array = audio_array.cpu().numpy().squeeze()
+
+sample_rate = model.generation_config.sample_rate
+scipy.io.wavfile.write("bark_out.wav", rate=sample_rate, data=audio_array)
