cf53ec01668fca3ef5521a1d8fef498ab54a9c34
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-10-05 10:16:38 -0500

    Created basic widgets and configured window

--- src/core/app.py
+++ src/core/app.py
@@ -1,25 +1,58 @@
-import tkinter as tk
 import customtkinter as ctk
 
 
 class App(ctk.CTk):
     def __init__(self):
+        super().__init__()
+
         # Set window properties
-        ctk.set_appearance_mode("System")
+        ctk.set_appearance_mode("Dark")
         ctk.set_default_color_theme("blue")
-        # self.geometry("800x600")
         self.title("IntelliCaster")
 
         # Create widgets
         self.create_widgets()
 
     def create_widgets(self):
-        self.lbl_title = ctk.CLabel(
+        # Create title label
+        self.lbl_title = ctk.CTkLabel(
             master=self,
             text="IntelliCaster",
-            font=("Arial", 20)
+            font=ctk.CTkFont(size=32, weight="bold")
         )
-        self.lbl_title.pack()
+        self.lbl_title.pack(padx=10, pady=10)
 
-    def run(self):
-        self.mainloop()
+        # Create message box
+        self.txt_messages = ctk.CTkTextbox(
+            master=self,
+            width=600,
+            height=200,
+            state="disabled",
+            font=ctk.CTkFont(size=14)
+        )
+        self.txt_messages.pack(padx=10, pady=10)
+
+        # Create start/stop button
+        self.btn_start_stop = ctk.CTkButton(
+            master=self,
+            text="⏵ Start Commentary",
+            width=300,
+            height=50,
+            font=ctk.CTkFont(size=18, weight="bold"),
+            command=self.start_stop
+        )
+        self.btn_start_stop.pack(padx=10, pady=10)
+    
+    def add_message(self, message):
+        self.txt_messages.configure(state="normal")
+        self.txt_messages.insert("0.0", message + "\n")
+        self.txt_messages.configure(state="disabled")
+    
+    def start_stop(self):
+        # Change button text
+        if self.btn_start_stop.cget("text") == "⏵ Start Commentary":
+            self.btn_start_stop.configure(text="⏹ Stop Commentary")
+        else:
+            self.btn_start_stop.configure(text="⏵ Start Commentary")
+
+        self.add_message("Start/Stop button pressed")
--- src/main.py
+++ src/main.py
@@ -2,4 +2,4 @@ from core.app import App
 
 if __name__ == "__main__":
     app = App()
-    app.run()
+    app.mainloop()
