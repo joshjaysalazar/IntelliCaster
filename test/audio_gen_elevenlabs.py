35316b04a207145b0fda12b2812aa6e7d99c7920
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-10-16 11:08:18 -0500

    Deleted outdated test files (all are implemented)

--- test/audio_gen_elevenlabs.py
+++
@@ -1,9 +0,0 @@
-from elevenlabs import generate, play
-
-audio = generate(
-  text="Hello! 你好! Hola! नमस्ते! Bonjour! こんにちは! مرحبا! 안녕하세요! Ciao! Cześć! Привіт! வணக்கம்!",
-  voice="Bella",
-  model="eleven_multilingual_v2"
-)
-
-play(audio)
--- test/toml_usage.py
+++
@@ -1,6 +0,0 @@
-import tomllib
-
-with open("test/test.toml", "rb") as f:
-    data = tomllib.load(f)
-
-print(data)
