71118f5f072e15eb77aae01603815e0d5caa12da
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-10-27 10:30:03 -0500

    Deleted implemented tests

--- test/get_drivers.py
+++
@@ -1,9 +0,0 @@
-import irsdk
-from pprint import pprint
-
-ir = irsdk.IRSDK()
-ir.startup()
-
-# Get the drivers
-drivers = ir['DriverInfo']['Drivers']
-pprint(drivers)
--- test/get_new_info.py
+++
@@ -1,9 +0,0 @@
-import irsdk
-from pprint import pprint
-
-ir = irsdk.IRSDK()
-ir.startup()
-
-last_laps = ir["CarIdxLapDistPct"]
-
-print(last_laps)
