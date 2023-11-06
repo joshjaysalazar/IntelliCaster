015b6a2ee82a9e6ea216fcbab75d671114fa6ca8
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-11-06 12:48:57 -0600

    Added docstrings to export classes

--- src/core/export.py
+++ src/core/export.py
@@ -3,7 +3,18 @@ from proglog import ProgressBarLogger
 
 
 class Export(ctk.CTkToplevel):
+    """Export window
+
+    The export window is a window that displays the progress of the video
+    export. It is a child window of the main window.
+    """
+
     def __init__(self, master):
+        """Constructor for the Export window
+
+        Args:
+            master (Tk): The parent window
+        """
         super().__init__(master)
 
         # Set window properties
@@ -25,6 +36,11 @@ class Export(ctk.CTkToplevel):
         self.progress_tracker = ProgressTracker(self.lbl_message, self.prg_bar)
 
     def create_widgets(self):
+        """Create widgets for the window
+        
+        Creates the widgets for the window and places them in the window. This
+        method is called by the constructor.
+        """
         # Create message label
         self.lbl_message = ctk.CTkLabel(
             self,
@@ -43,7 +59,19 @@ class Export(ctk.CTkToplevel):
         self.prg_bar.set(0)
 
 class ProgressTracker(ProgressBarLogger):
+    """Progress tracker
+
+    The progress tracker is a class that tracks the progress of the video
+    export. It is used by the export window to track the progress of the export.
+    """
+
     def __init__(self, message, progress):
+        """Constructor for the progress tracker
+
+        Args:
+            message (CTkLabel): The message label
+            progress (CTkProgressBar): The progress bar
+        """
         super().__init__()
 
         # Member variables
@@ -51,10 +79,30 @@ class ProgressTracker(ProgressBarLogger):
         self.progress = progress
 
     def bars_callback(self, bar, attr, value, old_value=None):
+        """Callback method for the progress tracker
+
+        This method is called by the progress tracker when the progress of the
+        export changes. It updates the progress bar with the current progress.
+
+        Args:
+            bar (str): The name of the progress bar
+            attr (str): The attribute that changed
+            value (int): The new value of the attribute
+            old_value (int, optional): The old value of the attribute. Defaults
+                to None.
+        """
         # Update progress bar
         self.progress.set(value / self.bars[bar]["total"])
 
     def callback(self, **changes):
+        """Callback method for the progress tracker
+
+        This method is called by the progress tracker when the progress of the
+        export changes. It updates the message label with the current progress.
+        
+        Args:
+            **changes (dict): A dictionary of changes
+        """
         for k, v in changes.items():
             text = self.format_text(v)
 
@@ -62,6 +110,17 @@ class ProgressTracker(ProgressBarLogger):
             self.message.configure(text=text)
 
     def format_text(self, text):
+        """Format text for the message label
+        
+        Formats the text for the message label. This method is called by the
+        callback method.
+        
+        Args:
+            text (str): The text to format
+            
+        Returns:
+            str: The formatted text
+        """
         # Convert to string if needed
         text = str(text)
 
