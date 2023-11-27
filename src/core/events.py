f1c43d2f4ee2490ed348fca26c1a40634e8d4eb1
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-11-27 10:30:50 -0600

    Added parameter to add method

--- src/core/events.py
+++ src/core/events.py
@@ -7,18 +7,20 @@ class Events:
         self.events = []
         self.id_counter = 0
 
-    def add(self, type, description):
+    def add(self, type, description, focus=None):
         """Add a new event to the list.
         
         Args:
             type (str): The type of event
             description (str): A description of the event
+            focus (str): The driver that the event is focused on
         """
         # Create a new event
         new_event = {
             "id": self.id_counter,
             "type": type,
             "description": description,
+            "focus": focus,
             "timestamp": time.time()
         }
 
