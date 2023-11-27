7253e689f4eeac2ffa998dfb8aa90e467b84b485
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-11-27 09:18:04 -0600

    Fixed an issue with the check function

--- src/core/common.py
+++ src/core/common.py
@@ -6,21 +6,31 @@ contains a function to check if iRacing is running.
 def check_iracing():
     """Check if iRacing is running.
 
+    Checks if iRacing is running by checking the connection status of the
+    IRSDK object. If iRacing is not running, the IRSDK object is shut down.
+    If iRacing is running, the IRSDK object is started up and the connection
+    is established.
+
     Returns:
         bool: True if iRacing is running, False otherwise.
     """
-    # Get iRacing connection status
-    initialized = ir.is_initialized
-    connected = ir.is_connected
+    # Try to start up the IRSDK object
+    try:
+        ir.startup()
 
-    # If iRacing is not running, shut down the SDK and return False
-    if not (initialized and connected):
+        # If the IRSDK object is initialized and connected, return True
+        if ir.is_initialized and ir.is_connected:
+            return True
+        
+        # Otherwise, shut down the IRSDK object and return False
+        else:
+            ir.shutdown()
+            return False
+
+    # If the IRSDK object cannot be started, return False
+    except:
         ir.shutdown()
         return False
-    
-    # If iRacing is running, return True
-    elif ir.startup() and initialized and connected:
-        return True
 
 # Contains the pointer to the root application window
 app = None
