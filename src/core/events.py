cba4e41db09840cf03eac8e5b146cbabbcd6d7ba
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-11-27 10:05:22 -0600

    Created the initial Events class

---
+++ src/core/events.py
@@ -0,0 +1,45 @@
+import time
+
+
+class Events:
+    def __init__(self):
+        # Initialize the events list and id counter
+        self.events = []
+        self.id_counter = 0
+
+    def add(self, type, description):
+        """Add a new event to the list.
+        
+        Args:
+            type (str): The type of event
+            description (str): A description of the event
+        """
+        # Create a new event
+        new_event = {
+            "id": self.id_counter,
+            "type": type,
+            "description": description,
+            "timestamp": time.time()
+        }
+
+        # Add the event to the list
+        self.events.insert(0, new_event)
+
+        # Increment the id counter
+        self.id_counter += 1
+
+    def clear(self, *types):
+        """Clear events from the list.
+        
+        Args:
+            *types (str): The types of events to clear
+        """
+        # If no type is given, clear all events
+        if len(types) == 0:
+            self.events = []
+            return
+
+        # If types are given, clear only those types
+        for event in self.events:
+            if event["type"] in types:
+                self.events.remove(event)
