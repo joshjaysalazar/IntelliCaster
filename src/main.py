49ed50056489037acc765e7897cf442d2c497e33
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2023-11-30 10:45:45 -0600

    Added timeout for splash screen

--- src/main.py
+++ src/main.py
@@ -9,10 +9,12 @@ def main():
     """Entry point for the application.
     
     This function is the entry point for the application. It creates the app
-    and starts the main loop.
+    and starts the main loop. It also creates the splash screen on a separate
+    thread which will be destroyed once the main window is created or after
+    10 seconds, whichever comes first.
     """
     # Create the splash screen on a separate thread
-    splash_screen = splash.SplashScreen("assets/splash.png")
+    splash_screen = splash.SplashScreen("assets/splash.png", timeout=10000)
     threading.Thread(target=splash_screen.mainloop)
 
     # Create the app
