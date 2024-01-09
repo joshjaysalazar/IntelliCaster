d1c074a57d7874e7004fbf21a56ef35c9e674a0c
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2024-01-09 14:06:52 -0600

    Removed cancel button, as it will require reworking vid rendering

--- src/core/export.py
+++ src/core/export.py
@@ -2,79 +2,6 @@ import customtkinter as ctk
 from proglog import ProgressBarLogger
 
 
-class CancelConfirm(ctk.CTkToplevel):
-    """Cancel confirmation window
-
-    The cancel confirmation window is a window that asks the user if they want
-    to cancel the video export. It is a child window of the export window.
-    """
-
-    def __init__(self, master):
-        """Constructor for the Cancel Confirmation window
-
-        Args:
-            master (CTk): The parent window
-        """
-        super().__init__(master)
-
-        # Set window properties
-        self.title("Cancel Export")
-        self.resizable(False, False)
-
-        # Reposition window
-        w = 400
-        h = 170
-        x = master.winfo_rootx() + (master.winfo_width() // 2) - (w // 2)
-        y = master.winfo_rooty() + (master.winfo_height() // 2) - (h // 2)
-        self.geometry(f"{w}x{h}+{x}+{y}")
-
-        # Create widgets
-        self._create_widgets()
-
-        # Bring window to front
-        self.lift()
-
-    def _confirm(self):
-        """Confirm cancel
-        
-        Confirms the cancel and destroys the export window.
-        """
-        # Destroy export window
-        self.master.destroy()
-
-        # Destroy cancel confirmation window
-        self.destroy()
-
-    def _create_widgets(self):
-        """Create widgets for the window
-        
-        Creates the widgets for the window and places them in the window. This
-        method is called by the constructor.
-        """
-        # Create message label
-        self.lbl_message = ctk.CTkLabel(
-            self,
-            text="Are you sure you want to cancel the export?",
-            font=ctk.CTkFont(size=14)
-        )
-        self.lbl_message.pack(pady=20)
-
-        # Create cancel button
-        self.btn_yes = ctk.CTkButton(
-            self,
-            text="Yes",
-            command=self._confirm
-        )
-        self.btn_yes.pack(pady=(0, 20))
-
-        # Create okay button
-        self.btn_no = ctk.CTkButton(
-            self,
-            text="No",
-            command=self.destroy
-        )
-        self.btn_no.pack(pady=(0, 20))
-
 class Export(ctk.CTkToplevel):
     """Export window
 
@@ -111,22 +38,11 @@ class Export(ctk.CTkToplevel):
         self.progress_tracker = ProgressTracker(
             self.lbl_message,
             self.prg_bar,
-            self.btn_cancel,
             self.btn_okay
         )
 
         # Bring window to front
         self.lift()
-
-    def _confirm_cancel(self):
-        """Confirm cancel
-        
-        Creates a cancel confirmation window and waits for the user to confirm
-        the cancel. If the user confirms the cancel, the export window is
-        destroyed by the cancel confirmation window.
-        """
-        # Create cancel confirmation window
-        CancelConfirm(self)
 
     def _create_widgets(self):
         """Create widgets for the window
@@ -151,20 +67,14 @@ class Export(ctk.CTkToplevel):
         self.prg_bar.pack(pady=(0, 20))
         self.prg_bar.set(0)
 
-        # Create cancel button
-        self.btn_cancel = ctk.CTkButton(
-            self,
-            text="Cancel",
-            command=self._confirm_cancel
-        )
-        self.btn_cancel.pack(pady=(0, 20))
-
-        # Create hidden okay button (don't pack it until the export is done)
+        # Create disabled okay button and pack it
         self.btn_okay = ctk.CTkButton(
             self,
             text="Okay",
+            state="disabled",
             command=self.destroy
         )
+        self.btn_okay.pack(pady=(0, 20))
 
 class ProgressTracker(ProgressBarLogger):
     """Progress tracker
@@ -173,7 +83,7 @@ class ProgressTracker(ProgressBarLogger):
     export. It is used by the export window to track the progress of the export.
     """
 
-    def __init__(self, message, progress, cancel, okay):
+    def __init__(self, message, progress, okay):
         """Constructor for the progress tracker
 
         Args:
@@ -185,7 +95,6 @@ class ProgressTracker(ProgressBarLogger):
         # Member variables
         self.message = message
         self.progress = progress
-        self.cancel = cancel
         self.okay = okay
 
     def _format_text(self, text):
@@ -223,11 +132,10 @@ class ProgressTracker(ProgressBarLogger):
         if text.startswith("Writing video "):
             text = "Exporting video file..."
 
-        # Custom final message and replace cancel button with okay button
+        # Custom final message and active the okay button
         if text.startswith("video ready "):
             text = "Video exported successfully!"
-            self.cancel.pack_forget()
-            self.okay.pack(pady=(0, 20))
+            self.okay.configure(state="normal")
 
         # Return the text
         return text
