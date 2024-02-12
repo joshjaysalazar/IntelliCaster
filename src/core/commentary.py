a36791ea99a1a20931b693a6316b7bb1e212b7f4
Joshua Abbott Salazar <joshjaysalazar@gmail.com>
2024-02-12 11:52:35 -0600

    Added emphasis to avoid repetition

--- src/core/commentary.py
+++ src/core/commentary.py
@@ -275,7 +275,7 @@ class TextGenerator:
         event_msg += "If two events are related, mention them together. "
         event_msg += "DO NOT mention the exact time of the event. "
         event_msg += "Use the lap distance to estimate the corner name/number. "
-        event_msg += "Do not repeat events that have already been mentioned. "
+        event_msg += "NEVER repeat events that have already been mentioned. "
         event_msg = {
             "role": "user",
             "content": event_msg
